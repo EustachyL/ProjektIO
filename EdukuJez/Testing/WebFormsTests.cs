4f5d7e5438f0cfe935b48532a1652e6810ab8c6e
EustachyL <57866931+EustachyL@users.noreply.github.com>
2024-06-11 01:05:14 +0200

    niedziałający test próbny

---
+++ EdukuJez/Testing/WebFormsTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+using Assert = Xunit.Assert;
+
+[TestClass]
+public class WebFormsTests : IDisposable
+{
+    private readonly IWebDriver _driver;
+
+    public WebFormsTests()
+    {
+        // Ustawienie ścieżki do ChromeDriver
+        _driver = new ChromeDriver();
+    }
+    [TestMethod]
+    [Fact]
+    public void HomePage_ShouldContainWelcomeText()
+    {
+        // Arrange
+        _driver.Navigate().GoToUrl("http://localhost:44309");
+
+        // Act
+        var element = _driver.FindElement(By.TagName("body"));
+
+        // Assert
+        Assert.Contains("Welcome", element.Text);
+    }
+
+    public void Dispose()
+    {
+        _driver.Quit();
+        _driver.Dispose();
+    }
+}
