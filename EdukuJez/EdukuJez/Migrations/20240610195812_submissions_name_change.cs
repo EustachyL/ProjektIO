50752f2c855ac0fdea1bb67af7493a78b988977b
EustachyL <57866931+EustachyL@users.noreply.github.com>
2024-06-10 21:59:30 +0200

    zmiana nazwy Substitutions

---
+++ EdukuJez/EdukuJez/Migrations/20240610195812_submissions_name_change.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EdukuJez.Migrations
+{
+    public partial class submissions_name_change : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Classes_Substitution_SubstitutionId",
+                table: "Classes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Substitution_Users_SubTeacherId",
+                table: "Substitution");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Substitution",
+                table: "Substitution");
+
+            migrationBuilder.RenameTable(
+                name: "Substitution",
+                newName: "Substitutions");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Substitution_SubTeacherId",
+                table: "Substitutions",
+                newName: "IX_Substitutions_SubTeacherId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Substitutions",
+                table: "Substitutions",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Classes_Substitutions_SubstitutionId",
+                table: "Classes",
+                column: "SubstitutionId",
+                principalTable: "Substitutions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Substitutions_Users_SubTeacherId",
+                table: "Substitutions",
+                column: "SubTeacherId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Classes_Substitutions_SubstitutionId",
+                table: "Classes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Substitutions_Users_SubTeacherId",
+                table: "Substitutions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Substitutions",
+                table: "Substitutions");
+
+            migrationBuilder.RenameTable(
+                name: "Substitutions",
+                newName: "Substitution");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Substitutions_SubTeacherId",
+                table: "Substitution",
+                newName: "IX_Substitution_SubTeacherId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Substitution",
+                table: "Substitution",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Classes_Substitution_SubstitutionId",
+                table: "Classes",
+                column: "SubstitutionId",
+                principalTable: "Substitution",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Substitution_Users_SubTeacherId",
+                table: "Substitution",
+                column: "SubTeacherId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- EdukuJez/EdukuJez/Model/ServerAccess/DBEntity/Substitution.cs
+++ EdukuJez/EdukuJez/Model/ServerAccess/DBEntity/Substitution.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace EdukuJez.Repositories
 {
+    [Table("Substitutions")]
     public class Substitution : EntityBase
     {
         public string Desc { get; set; }
